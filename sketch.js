c929458509f676bb7663c72863d2bf6df1df45a0
suhaask31 <92918926+suhaask31@users.noreply.github.com>
2021-12-15 19:36:49 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var student1
+
+function setup() {
+  createCanvas(400,400);
+  student1 = new Student("Name: Bob","Age: 13","Grade: 8")
+  student1.show() 
+  
+}
+
+function draw() 
+{
+  background(30);
+ 
+}
---
+++ student.js
@@ -0,0 +1,18 @@
+
+class Student
+{
+    constructor(name,age,grade){
+        this.name = name ; 
+        this.age = age ; 
+        this.grade = grade ; 
+    }
+    show(){
+        console.log(this.name)
+        console.log(this.age)
+        console.log(this.grade)
+    }
+
+}
+
+
+
